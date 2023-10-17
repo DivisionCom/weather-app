186572a66a00f15175607ebe988fd02211533465
DivisionCom <xd6511@gmail.com>
2023-10-17 15:59:58 +0300

    Add WeatherModel data class & CardView

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.example.weatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp.adapters.WeatherModel
 
 class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
---
+++ app/src/main/java/com/example/weatherapp/adapters/WeatherModel.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.adapters
+
+data class WeatherModel(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val imageUrl: String,
+    val hours: String
+)
