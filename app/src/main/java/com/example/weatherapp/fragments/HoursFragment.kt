527a3ba53ca1be90227d1dd741d8e5b5fc90461e
DivisionCom <xd6511@gmail.com>
2023-10-17 17:05:57 +0300

    Add func initRcView in HoursFragment

--- app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp.R
+import com.example.weatherapp.adapters.WeatherAdapter
+import com.example.weatherapp.adapters.WeatherModel
+import com.example.weatherapp.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
-
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        // test
+        val list = listOf(
+            WeatherModel("",
+                "12:00", "Sunny","25℃",
+                "","","",""),
+            WeatherModel("",
+                "13:00", "Sunny","25℃",
+                "","","",""),
+            WeatherModel("",
+                "14:00", "Sunny","35℃",
+                "","","","")
+        )
+        // test
+        adapter.submitList(list)
     }
 
     companion object {
